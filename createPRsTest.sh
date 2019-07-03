41c5f709e8ce8ff4ed15a77a662d97f880d71a5d
Jeffrey Barto <bartorobjeff@gmail.com>
2019-07-03 13:44:57 -0400

    Adding file test1

--- createPRsTest.sh
+++ createPRsTest.sh
@@ -1,9 +1,9 @@
-for i in {1..55}; do
+for i in {1..1}; do
     git checkout -b mybranch$i
     echo example > test$i
     git add .
     git commit -m "Adding file test$i"
     git push --set-upstream origin mybranch$i
-    git request-pull master origin mybranch$i
+    git request-pull origin/master origin mybranch$i
     echo "CREATED PR"
 done
